b7c8ba4371c1b66eca34d1e9eec964592150e854
endsun1234 <endsun1234@naver.com>
2025-05-19 16:43:48 +0900

    2025.05.19

--- Assets/02_Scripts/Example_IfElse.cs
+++ Assets/02_Scripts/Example_IfElse.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Example_IfElse : MonoBehaviour
 {
     public int a = 0;
+    public int hp = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (a == 0)    // 만약에 a가 0과 같다라면
+        {
+            print("a는 0입니다!!!");
+        }
+
+        else if (a > 0)
+        {
+            print("a는 0보다 큽니다...");
+        }
+
+        else if (a < 0)
+        {
+            print("a는 0보다 작습니다...");
+        }
+
+        else
+        {
+            print("a는 0이 아닙니다...");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hp <= 0)
+        {
+            print("You Died...");
+        }
     }
 }
